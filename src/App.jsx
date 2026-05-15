0819c23d827bf40aac3bde3f9d12f0887a382f88
Bishek Yadav <bishek@lpu.in>
2026-05-16 00:29:21 +0530

    fix: route all paths to download and fix background layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Landing from './pages/Landing';
-import About from './pages/About';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Otp from './pages/Otp';
-import Home from './pages/Home';
-import BuyArb from './pages/BuyArb';
-import Payment from './pages/Payment';
-import Plan from './pages/Plan';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Download from './pages/Download';
 import ScrollToTop from './components/ScrollToTop';
-import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/otp" element={<Otp />} />
-        <Route path="/plan" element={<Plan />} />
         <Route path="/download" element={<Download />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/buy-arb" element={<BuyArb />} />
-          {/* Note: I did not protect /payment here as it handles its own logic, 
-              but let me know if it should be protected too! */}
-        </Route>
-        <Route path="/payment" element={<Payment />} />
+        {/* Redirect all other routes to the download page */}
+        <Route path="*" element={<Navigate to="/download" replace />} />
       </Routes>
     </BrowserRouter>
   );
